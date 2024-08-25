6f2736c6be23e49af879324021096a72687e0baf
Nithish1018 <nithishkumar.sileveri123@gmail.com>
2024-08-25 10:55:21 +0530

    updating config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port: 3000,
-    proxy:{
-      "/api":{
-        target:"https://rahuk-test.onrender.com",
-      }
-    }
-  }
-})
+});
